6e9c937d255e2128775d2c2d1b5cbab9f40610b8
Sabrina <sabrinaprogram99@gmail.com>
2023-09-06 23:31:41 -0300

    portfolio

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,6 +1,6 @@
 import { SectionTitle } from "../sectionTitle/sectionTitle"
 import './experience.scss'
-
+import Image from 'next/image'
 interface ExperienceProps {
 
 }
@@ -15,13 +15,76 @@ export function Experience() {
 
                 <div className="experience-time">
 
+                    <div className="experience-language">
+                        <Image
+                            src="/java.png"
+                            alt="Java Logo"
+                            width={50}
+                            height={50}
+                            priority
+                        />
+
+                        <div className="experience-unit">
+                            <div className="experience-measure measure-0">
+                            <span>6 months</span>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div className="experience-language">
+                        <Image
+                            src="/react.png"
+                            alt="React Logo"
+                            width={50}
+                            height={50}
+                            priority
+                        />
+                        <div className="experience-unit">
+                            <div className="experience-measure measure-0">
+                            <span>6 months</span>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div className="experience-language ">
+                        <Image
+                            src="/ts.png"
+                            alt="Typescript Logo"
+                            width={50}
+                            height={50}
+                            priority
+                        />
+                        <div className="experience-unit">
+                            <div className="experience-measure measure-0">
+                            <span>6 months</span>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div className="experience-language">
+                        <Image
+                            src="/js.png"
+                            alt="JavaScript Logo"
+                            width={50}
+                            height={50}
+                            priority
+                        />
+
+                        <div className="experience-unit">
+                            <div className="experience-measure measure-0">
+                                <span>6 months</span>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
-
-
-
-
-
         </>
     )
 }
